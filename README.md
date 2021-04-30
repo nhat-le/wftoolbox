e72ae44256c213cae2d1dc42b01cf4ec75e2f314
Nhat Le <nmle@mit.edu>
2021-04-30 11:00:39 -0400

    Added script for brain template from locanmf

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A toolbox for widefield imaging analysis
 - Download the TIFFStack toolbox according to the instructions at https://github.com/DylanMuir/TIFFStack
 - Download musall WideFieldImager code: download the ``Analysis'' folder from https://github.com/musall/WidefieldImager/tree/master/Analysis and add to the MATLAB path
 - Download the wftoolbox (this repositiory) or clone from Github and add to the MATLAB path
+
+### Instructions for setting up animal templates
+- Download the locaNMF-prepocess toolbox from https://github.com/ss5513/locaNMF-preprocess
+- Add the locaNMF/utils folder to the MATLAB path
+- Open wftoolbox/makeBrainTemplate and follow the instructions
---
+++ makeBrainTemplate.m
@@ -0,0 +1,75 @@
+%% Change the directory of this path to locanmf-preprocess/utils
+addpath('/Users/minhnhatle/Documents/ExternalCode/locaNMF-preprocess/utils/');
+
+%% Load the overlaid image
+% For E53
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/e53Template/';
+% Y = imread(fullfile(fdir, 'serenoOverlay.png'));
+
+% For E54
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/e54Template/';
+% Y = imread(fullfile(fdir, 'serenoOverlay.png'));
+
+% For E57
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/e54Template/';
+% Y = imread(fullfile(fdir, 'serenoOverlay.png'));
+
+% For F01
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/f01Template/';
+% Y = imread(fullfile(fdir, 'f01surface.png'));
+
+% For F02
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/f02Template/';
+% Y = imread(fullfile(fdir, 'f02surface.tif'));
+
+% For F03
+fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/f03Template/';
+Y = imread(fullfile(fdir, 'f03surface.tif'));
+
+% For F04
+% fdir = '/Users/minhnhatle/Dropbox (MIT)/Sur/2p1/f04Template/';
+% Y = imread(fullfile(fdir, 'f04surface.png'));
+
+%% Get brainmask
+% can set to 'docked' if we prefer
+% set(0,'DefaultFigureWindowStyle','normal'); warning('off','images:imshow:magnificationMustBeFitForDockedFigure')
+
+% fprintf('Click the vertices to define a brainmask.\nRight click to finish and close polygon.\nDouble-click inside polygon to accept it.\n');
+% % R=roipoly(max(Y,[],3)./max(Y(:)));
+% R = roipoly(Y);
+% % For a circular roi, use the code below
+% % imshow(Y);
+% % R = images.roi.Circle(gca, 'Center', [300 300], 'Radius', 100);
+% brainmask=double(R);
+% brainmask(brainmask==0)=NaN;
+% Y=repmat(brainmask,1,1,size(Y,3)).*double(Y);
+% imshow(max(Y,[],3)./max(Y(:)))
+
+%% Align data to atlas + get inverse atlas
+load('allenDorsalMap.mat')
+atlas = dorsalMaps.dorsalMapScaled;
+
+% Multiply left side by -1 to have side information
+[xx,yy] = meshgrid(1:size(atlas, 1), 1:size(atlas, 2));
+atlas(xx < size(atlas, 1) / 2) = atlas(xx < size(atlas, 1) / 2) * -1;
+
+borders = dorsalMaps.edgeMapScaled;
+load('utils/atlasStandard.mat', 'areanames');
+% tform = align_recording_to_allen(max(Y,[],3), {'R VISp1'}); % align <-- input any function of data here
+tform = align_recording_to_allen(max(Y,[],3));
+invT=pinv(tform.T); % invert the transformation matrix
+invT(1,3)=0; invT(2,3)=0; invT(3,3)=1; % set 3rd dimension of rotation artificially to 0
+invtform=tform; invtform.T=invT; % create the transformation with invtform as the transformation matrix
+atlas=imwarp(atlas,invtform,'interp','nearest','OutputView',imref2d(size(Y(:,:,1)))); % setting the 'OutputView' is important
+borders=imwarp(borders,invtform,'interp','nearest','OutputView',imref2d(size(Y(:,:,1)))); % setting the 'OutputView' is important
+atlas=round(atlas);
+borders = round(borders);
+%% Plot the warped atlas
+figure; subplot(1,2,1); imagesc(max(Y,[],3) + uint16(borders) * 10000); axis image
+% hold on
+% h = imshow(borders * 255);
+% set(h, 'AlphaData', 0.4);
+subplot(1,2,2); imagesc(atlas); axis image
+
+%% Save the warped atlas
+save(fullfile(fdir,'atlas_F03.mat'),'atlas','areanames','invtform', 'borders');
